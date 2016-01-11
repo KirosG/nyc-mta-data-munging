ffefa7c40b3e25ed71475fe364e9c79158886673
Justin Stewart <justinstewart.la@gmail.com>
2016-01-11 16:49:48 -0500

    finished challenge 2

--- challenge2.py
+++ challenge2.py
@@ -1,20 +1,17 @@
-import pandas as pd
-import numpy as np
 import csv
 from collections import defaultdict
-from dateutil import parser
+import dateutil.parser
 
-def mta_csv_to_dict(path_to_csv):
+def mta_csv_to_timeseries(path_to_csv):
 	with open(path_to_csv, 'rb') as csvfile:
 	    mta_reader = csv.reader(csvfile, delimiter=',')
 	    next(mta_reader, None)
-	    
+	   
 	    mta_dict = defaultdict(list)
 	    for row in mta_reader:
 	    	row = [element.strip() for element in row]
-	        key, val =  tuple(row[:4]), row[4:]
-	        mta_dict[key].append(val)
+	        key, date, time, entries =  tuple(row[:4]), row[6], row[7], row[9]
+	        mta_dict[key].append([date,time,entries])
 	return dict(mta_dict)
 
-mta_dict = mta_csv_to_dict('mta_data_jan09.csv')
-
+# mta = mta_csv_to_timeseries('mta_data_jan09.csv')
