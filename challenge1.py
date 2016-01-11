fc64af6f34d135782140584eca7ba6bd07e1b551
Justin Stewart <justinstewart.la@gmail.com>
2016-01-11 16:22:03 -0500

    finished challege1 by creating a fn to create dicts from mta data

--- challenge1.py
+++ challenge1.py
@@ -1,15 +1,19 @@
-# Download a few MTA turnstile data files
-# Open up a file, use csv reader to read it, 
-# make a python dict where there is a key for each (C/A, UNIT, SCP, STATION).
-# These are the first four columns. 
-# The value for this key should be a list of lists. 
-# Each list in the list is the rest of the columns in a row. 
-
-
 import pandas as pd
+import numpy as np
+import csv
+from collections import defaultdict
 
-df = pd.read_csv('mta_data_jan09.csv')
-df.head()
+def mta_csv_to_dict(path_to_csv):
+	with open(path_to_csv, 'rb') as csvfile:
+	    mta_reader = csv.reader(csvfile, delimiter=',')
+	    next(mta_reader, None)
+	    
+	    mta_dict = defaultdict(list)
+	    for row in mta_reader:
+	    	row = [element.strip() for element in row]
+	        key, val =  tuple(row[:4]), row[4:]
+	        mta_dict[key].append(val)
+	return dict(mta_dict)
 
-
+mta_dict = mta_csv_to_dict('mta_data_jan09.csv')
 
