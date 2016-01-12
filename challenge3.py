6f488383371579bb6d7889ca090ed19f9560669f
Justin Stewart <justinstewart.la@gmail.com>
2016-01-12 15:48:00 -0500

    added new fn to de-cumulate (reverse cummulative value into single daily totals)

--- challenge3.py
+++ challenge3.py
@@ -1,5 +1,6 @@
 from challenge2 import mta 
 import dateutil.parser
+import datetime
 
 
 k = mta.keys()[0]
@@ -12,3 +13,13 @@ for i in ts:
     else:
         d[i[0].date()] = min(i[1],d[i[0].date()])
 
+def decumulate(mta_dict):
+	new_dict = {}
+	for day in mta_dict.keys():
+		next_day = day + datetime.timedelta(days=1)
+		if next_day not in mta_dict:
+			day_to_delete = day
+		else:
+			new_value = int(mta_dict[next_day]) - int(mta_dict[day])
+			new_dict[day] = new_value
+	return new_dict
