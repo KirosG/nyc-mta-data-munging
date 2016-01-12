3ed4c1b5b725df2ed70e194cd270a03757cb4f38
Justin Stewart <justinstewart.la@gmail.com>
2016-01-12 12:51:55 -0500

    fixed datetime parser

--- challenge2.py
+++ challenge2.py
@@ -10,8 +10,8 @@ def mta_csv_to_timeseries(path_to_csv):
 	    mta_dict = defaultdict(list)
 	    for row in mta_reader:
 	    	row = [element.strip() for element in row]
-	        key, date, time, entries =  tuple(row[:4]), row[6], row[7], row[9]
-	        mta_dict[key].append([date,time,entries])
+	        key, datetime, entries =  tuple(row[:4]), dateutil.parser.parse(row[6] +' '+ row[7]), row[9]
+	        mta_dict[key].append([datetime,entries])
 	return dict(mta_dict)
 
 mta = mta_csv_to_timeseries('mta_data_jan09.csv')
